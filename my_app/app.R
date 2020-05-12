815cc4258bcdef868f5d8c8705731643f39b3017
bhatt-keshav <keshavbhatt@outlook.com>
2020-05-12 23:58:02 +0200

    lesson 3 widgets model answer

--- my_app/app.R
+++ my_app/app.R
@@ -2,32 +2,27 @@ library(shiny)
 
 # Define UI ----
 ui <- fluidPage(
-  titlePanel(h1("censusVis")),
-  fluidRow(
-    column(3,
-           p("Create demographic maps with information form the 2010 US 
-             census"),
-           br(), 
-           selectInput("select", strong("Choose a variable to display"), 
-                       choices = list("Pc white" = 1, "Pc black" = 2,
-                                      "Pc hisp" = 3, "Pc Asian" = 4)
-                       , selected = 1)),
-    br(),
-    sliderInput("slider1", strong("Range of interest"),
-                min = 0, max = 100, value = 0)
-  ),
-  fluidRow(
-    br(), br(), br(), br(), 
-    column(5, 
-           selectInput("select1", ("Choose a var to disp"), 
-                       choices = list("Pc white" = 1, "Pc black" = 2,
-                                      "Pc hisp" = 3, "Pc Asian" = 4)
-                       , selected = 1)),
-    column(5, 
-           selectInput("select1", ("Choose a var to disp"), 
-                       choices = list("Pc white" = 1, "Pc black" = 2,
-                                      "Pc hisp" = 3, "Pc Asian" = 4)
-                       , selected = 1))
+  titlePanel("censusVis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with 
+               information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = list("Percent White", 
+                                 "Percent Black",
+                                 "Percent Hispanic", 
+                                 "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+    
+    mainPanel()
   )
 )
 # Define server logic ----
